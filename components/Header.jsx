a8a1e3ebeeddb39439cbc7487412bd2cb25aac9f
Amarachukwu Ogbu <84595051+amikable0179@users.noreply.github.com>
2022-11-15 18:03:50 +0100

    Initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h2>WOW! it worked!!</h2>
+    </div>
+  );
+}
---
+++ components/Index.jsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div>
+      <h1> Hello world</h1>
+      <h2>you are to</h2>
+    </div>
+  );
+}
